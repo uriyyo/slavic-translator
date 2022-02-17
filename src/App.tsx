0f70ce4cb04165a71a9a3fe8fac9895130b94c57
RomanTokar <romantokar.biz@gmail.com>
2022-02-17 10:42:04 +0200

    Changed ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,134 @@
-import './App.css';
-import {useState} from "react"
-import * as React from "react";
-import {Paper, Button, TextField, Stack, Box} from "@mui/material"
-import {Groups, TranslationResult} from "./types";
-import {LANGUAGES, SERVER_URL} from "./consts"
-
+import './App.css'
+import * as React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CircularProgress,
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+} from '@mui/material'
+import { Groups, TranslationResult } from './types'
+import { LANGUAGES, SERVER_URL } from './consts'
+import { debounceTime, from, fromEvent, map, switchMap, tap } from 'rxjs'
 
 async function translate(text: string): Promise<Array<TranslationResult>> {
-    return [
-        {"src": "en", "dest": "bg", "text": "Hello", "translation": "Здравейте"},
-        {"src": "en", "dest": "be","text": "Hello", "translation": "добры дзень"},
-        {"src": "en", "dest": "bs", "text": "Hello", "translation": "zdravo"},
-        {"src": "en", "dest": "hr", "text": "Hello", "translation": "zdravo"},
-        {"src": "en", "dest": "cs", "text": "Hello", "translation": "Ahoj"},
-        {"src": "en", "dest": "mk","text": "Hello", "translation": "Здраво"},
-        {"src": "en", "dest": "pl", "text": "Hello", "translation": "Witaj"},
-        {"src": "en", "dest": "ru", "text": "Hello", "translation": "Привет"},
-        {"src": "en", "dest": "sr", "text": "Hello", "translation": "Здраво"},
-        {"src": "en", "dest": "sk", "text": "Hello", "translation": "Ahoj"},
-        {"src": "en", "dest": "sl", "text": "Hello", "translation": "zdravo"},
-        {"src": "en", "dest": "uk", "text": "Hello", "translation": "Здрастуйте"}
-    ]
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        { src: 'en', dest: 'bg', text: 'Hello', translation: 'Здравейте' },
+        { src: 'en', dest: 'be', text: 'Hello', translation: 'добры дзень' },
+        { src: 'en', dest: 'bs', text: 'Hello', translation: 'zdravo' },
+        { src: 'en', dest: 'hr', text: 'Hello', translation: 'zdravo' },
+        { src: 'en', dest: 'cs', text: 'Hello', translation: 'Ahoj' },
+        { src: 'en', dest: 'mk', text: 'Hello', translation: 'Здраво' },
+        { src: 'en', dest: 'pl', text: 'Hello', translation: 'Witaj' },
+        { src: 'en', dest: 'ru', text: 'Hello', translation: 'Привет' },
+        { src: 'en', dest: 'sr', text: 'Hello', translation: 'Здраво' },
+        { src: 'en', dest: 'sk', text: 'Hello', translation: 'Ahoj' },
+        { src: 'en', dest: 'sl', text: 'Hello', translation: 'zdravo' },
+        { src: 'en', dest: 'uk', text: 'Hello', translation: 'Здрастуйте' },
+      ])
+    }, 200)
+  })
 
-    let payload = Object.keys(LANGUAGES).map(dest => ({
-        text,
-        dest,
-        src: "en",
-    }))
+  let payload = Object.keys(LANGUAGES).map(dest => ({
+    text,
+    dest,
+    src: 'en',
+  }))
 
-    let response = await fetch(
-        `${SERVER_URL}/translate`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
-        }
-    );
+  let response = await fetch(`${SERVER_URL}/translate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
 
-    return await response.json() as Array<TranslationResult>
+  return (await response.json()) as Array<TranslationResult>
 }
 
 function constructTranslationResult(translations: any): any {
-    let groups: Record<Groups, Array<any>> = {
-        [Groups.EAST]: [],
-        [Groups.WEST]: [],
-        [Groups.SOUTH]: [],
-    }
+  let groups: Record<Groups, Array<any>> = {
+    [Groups.EAST]: [],
+    [Groups.WEST]: [],
+    [Groups.SOUTH]: [],
+  }
 
-    for (let {translation, dest} of translations)
-        groups[LANGUAGES[dest].group].push({translation, dest})
+  for (let { translation, dest } of translations)
+    groups[LANGUAGES[dest].group].push({ translation, dest })
 
-    // @ts-ignore
-    return [...Object.entries(groups)]
+  // @ts-ignore
+  return [...Object.entries(groups)]
 }
 
 function App() {
-    let [groups, setGroups] = useState([])
+  const [groups, setGroups] = useState([])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [loading, setLoading] = useState(false)
 
-    const callback = async () => {
-        let inp = document.getElementById("translation")
+  const callback = async value => {
+    const result = await translate(value)
+    return constructTranslationResult(result)
+  }
 
-        // @ts-ignore
-        let result = await translate(inp.value)
-        let converted = constructTranslationResult(result)
-        // @ts-ignore
-        setGroups(converted);
-    }
+  useEffect(() => {
+    const subscription = fromEvent<Event>(inputRef.current, 'input')
+      .pipe(
+        debounceTime(100),
+        map(() => inputRef.current.value),
+        tap(() => setLoading(true)),
+        switchMap(value => from(callback(value)))
+      )
+      .subscribe(groups => {
+        setGroups(groups)
+        setLoading(false)
+      })
 
-    return (
-        <Box sx={{mx: "auto"}}>
-            <Button variant="outlined" onClick={callback}>Translate</Button>
-            <TextField id="translation" label="Outlined" variant="outlined"/>
-            <Stack direction="row" spacing={5}>
-                {
-                    groups.map(
-                        ([group, translations]) => (
-                            <Paper>
-                                <text>{group}</text>
-                                <div>
-                                    {translations.map(({translation, dest}) => (
-                                        <div key={dest}>
-                                            <span>{LANGUAGES[dest].flag} {LANGUAGES[dest].name} :</span>
-                                            <span>{translation}</span>
-                                        </div>
-                                    ))}
-                                </div>
-                            </Paper>
-                        )
-                    )
-                }
-            </Stack>
-        </Box>
-    );
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return (
+    <Container maxWidth={'md'}>
+      <TextField
+        inputRef={inputRef}
+        sx={{ mb: 2 }}
+        label="Translate"
+        variant="outlined"
+        fullWidth
+        InputProps={{
+          endAdornment: loading && (
+            <InputAdornment position="end">
+              <CircularProgress size={20} />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Grid container spacing={2}>
+        {groups.map(([group, translations]) => (
+          <Grid item xs={4} key={group}>
+            <Card sx={{ height: '100%' }}>
+              <CardHeader title={group} />
+              <CardContent>
+                {translations.map(({ translation, dest }) => (
+                  <div key={dest}>
+                    <span>
+                      {LANGUAGES[dest].flag} {LANGUAGES[dest].name} :
+                    </span>
+                    <span>{translation}</span>
+                  </div>
+                ))}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
 }
 
-export default App;
+export default App
